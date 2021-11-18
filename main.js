25eed0cdbbd0cfbf484bd78c4d7ccdaf2837c39c
procoder2011 <87647462+procoder2011@users.noreply.github.com>
2021-11-18 16:31:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name = "Ranbir";
+console.log(first_name);
+var last_name = "Kapoor";
+console.log(last_name);
+var a = 10;
+var b = 5;
+var add = a + b ;
+console.log(add);
+var multiply = a * b ;
+console.log(multiply)
+var divide = a / b ;
+console.log(divide)
